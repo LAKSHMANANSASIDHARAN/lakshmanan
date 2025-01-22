bf80d87859452486a9f76ea893a753bcd6288bd9
LAKSHMANANSASIDHARAN <d45624312@gmail.com>
2025-01-22 20:30:34 +0530

    Update codingday4.cpp

--- codingday4.cpp
+++ codingday4.cpp
@@ -37,3 +37,45 @@
     }
     return arr; // return the modified array.
     } 
+// Final Value of Variable After Performing Operations-Leetcode[2011]
+/*There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+*/
+//Thats what this problem stats!!!!!!
+// Logic is very simple !!
+// they mentioned that language has only four opertions and one variable ,by that we say two increment and two decrement
+// so take one variable assign that value as 0 then according to the opertion in array you can 
+//increment or decrement the x .
+// return that final answer.
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0,i=0;
+        while(i<operations.size()){
+            if(operations[i]=="X++" or operations[i]=="++X"){
+                x=x+1;
+            }
+            else{
+                x=x-1;
+            }
+            i++;
+        }
+        return x;
+       }
+};
+
