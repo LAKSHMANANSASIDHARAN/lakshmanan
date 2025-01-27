feee3d939e498cb4d6f86380b336e5d8a8aa25dc
S.Lakshmanan <d45624312@gmail.com>
2025-01-27 22:11:12 +0530

    Update codingday8.cpp

--- codingday8.cpp
+++ codingday8.cpp
@@ -1,4 +1,4 @@
-/*Today problem is Minimum number of operations to move all balls to each box
+/*1.Today problem is Minimum number of operations to move all balls to each box
 They have given the binary string , if the string is 0 box is empty ,if its 1 box has ball .
 we need to calculate the distance of ball to move to the each boxes*/
 /*Input: boxes = "110"
@@ -23,3 +23,57 @@ vector<int> minOperations(string boxes) {
 
 // time complexity is O(n^2)
 // space complexity is O(1)
+/*2.Second problem states they have given the array and k value . We need to find the sum k element in the array and store that 
+ in new array . 
+ For example:
+ Input: code = [5,7,1,4], k = 3
+Output: [12,10,16,13]
+Explanation: Each number is replaced by the sum of the next 3 numbers. 
+The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. Notice that the numbers wrap around.
+if k is negative we need to add in reverse manner:
+Input: code = [2,4,9,3], k = -2
+Output: [12,5,6,13]
+Explanation: The decrypted code is [3+9, 2+3, 4+2, 9+4]. Notice that the numbers wrap around again. 
+If k is negative, the sum is of the previous numbers.
+ */
+/*But the task is implementation of code for both condition */
+int i, j;
+vector<int> ans;
+int n = code.size();
+        if (k > 0) { // if k positive i is 1 and j is k
+    i = 1;
+    j = k;
+} 
+else { // if it negative i is size of array - k and j is size of array -1
+    i = n + k;  
+    j = n - 1;  
+}
+int sum = 0;
+while (i <= j) { // first add the k element in array
+    sum = sum + code[i % n];
+    i++;
+}
+
+if (k > 0) {
+    i = 1;
+    j = k;
+} else {
+    i = n + k;
+    j = n - 1;
+}
+// re-initialzing
+ans.push_back(sum);
+int remaining = n - 1;
+// use the sliding window algorithm
+while (remaining) {
+    sum = sum - code[i % n];
+    sum = sum + code[(j + 1) % n];
+    ans.push_back(sum);
+    remaining--;
+    i++;
+    j++;
+}
+return ans;
+    }
+// time complexity is O(n)
+//space complexity is 0(1);
