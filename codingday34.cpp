79221760a5f0456b6739b5cc24bd10fd8591936a
S.Lakshmanan <d45624312@gmail.com>
2025-02-26 20:05:47 +0530

    Update codingday34.cpp

--- codingday34.cpp
+++ codingday34.cpp
@@ -85,3 +85,51 @@ public:
        return ans;
     }
 };
+/*Max Increase to Keep City Skyline
+There is a city composed of n x n blocks, where each block contains a single building shaped like a vertical square prism. You are given a 0-indexed n x n integer matrix grid where grid[r][c] represents the height of the building located in the block at row r and column c.
+A city's skyline is the outer contour formed by all the building when viewing the side of the city from a distance. The skyline from each cardinal direction north, east, south, and west may be different.
+We are allowed to increase the height of any number of buildings by any amount (the amount can be different per building). The height of a 0-height building can also be increased. However, increasing the height of a building should not affect the city's skyline from any cardinal direction.
+Return the maximum total sum that the height of the buildings can be increased by without changing the city's skyline from any cardinal direction.
+Example 1:
+Input: grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+Output: 35
+Explanation: The building heights are shown in the center of the above image.
+The skylines when viewed from each cardinal direction are drawn in red.
+The grid after increasing the height of buildings without affecting skylines is:
+gridNew = [ [8, 4, 8, 7],
+            [7, 4, 7, 7],
+            [9, 4, 8, 7],
+            [3, 3, 3, 3] ]
+Example 2:
+Input: grid = [[0,0,0],[0,0,0],[0,0,0]]
+Output: 0
+Explanation: Increasing the height of any building will result in the skyline changing.*/
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        vector<int> row, coloum;
+        for (int i = 0; i < grid.size(); i++) {
+            int maxi = 0;
+            for (int j = 0; j < grid[0].size(); j++) {
+                maxi = max(maxi, grid[i][j]);
+            }
+            row.push_back(maxi);
+        }
+        for (int i = 0; i < grid.size(); i++) {
+            int maxi = 0;
+            for (int j = 0; j < grid[0].size(); j++) {
+                maxi = max(maxi, grid[j][i]);
+            }
+            coloum.push_back(maxi);
+        }
+        int result = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                int maxHeight = min(row[i], coloum[j]);
+                result += maxHeight - grid[i][j];
+            }
+        }
+       return result;
+    }
+};
