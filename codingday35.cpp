d107761897bb11df34e40ed72880f49b748fa90d
S.Lakshmanan <d45624312@gmail.com>
2025-02-27 20:37:58 +0530

    Create codingday35.cpp

---
+++ codingday35.cpp
@@ -0,0 +1,34 @@
+/*Minimum Sum of Four Digit Number After Splitting Digits
+You are given a positive integer num consisting of exactly four digits. Split num into two new integers new1 and new2 by using the digits found in num. Leading zeros are allowed in new1 and new2, and all the digits found in num must be used.
+For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. Some of the possible pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
+Return the minimum possible sum of new1 and new2.
+Example 1:
+Input: num = 2932
+Output: 52
+Explanation: Some possible pairs [new1, new2] are [29, 23], [223, 9], etc.
+The minimum sum can be obtained by the pair [29, 23]: 29 + 23 = 52.
+Example 2:
+Input: num = 4009
+Output: 13
+Explanation: Some possible pairs [new1, new2] are [0, 49], [490, 0], etc. 
+The minimum sum can be obtained by the pair [4, 9]: 4 + 9 = 13.*/
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int>temp;
+        while(num>0){
+            int t=0;
+            t=num%10;
+            temp.push_back(t);
+            num=num/10;
+        }
+        sort(temp.begin(),temp.end());
+        int pair_1=0,pair_2=0;
+        int i=0;
+    
+           pair_1=temp[0]*10 +temp[3];
+           pair_2=temp[1]*10+temp[2];
+           return pair_1+pair_2;
+        
+    }
+};
