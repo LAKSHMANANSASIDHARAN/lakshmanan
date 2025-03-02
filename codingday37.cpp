09a1effa694e5d5c70db7f38f18bb51904397d5b
S.Lakshmanan <d45624312@gmail.com>
2025-03-02 21:37:21 +0530

    Update codingday37.cpp

--- codingday37.cpp
+++ codingday37.cpp
@@ -78,3 +78,40 @@ public:
         return ans;
     }
 };
+/*Subsets
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.*/
+class Solution {
+public:
+    void subset(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp,
+                int i, int n) {
+        if (i >= n) {
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        subset(nums, ans, temp, i + 1, n);
+        temp.pop_back();
+        subset(nums, ans, temp, i + 1, n);
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        int i = 0;
+        int n = nums.size();
+        subset(nums, ans, temp, 0, n);
+        return ans;
+    }
+};
