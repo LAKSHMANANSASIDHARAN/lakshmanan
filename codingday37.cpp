26ccf95d7d62d5f1715772e108399696b883c6bf
S.Lakshmanan <d45624312@gmail.com>
2025-03-02 21:36:05 +0530

    Update codingday37.cpp

--- codingday37.cpp
+++ codingday37.cpp
@@ -39,3 +39,42 @@ public:
         return message;
     }
 };
+/*Permutations
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+ Constraints:
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.*/
+class Solution {
+public:
+    void permutations(vector<int>& nums, vector<vector<int>>& ans, int i,
+                      int n) {
+        if (i == n) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int j = i; j < n; j++) {
+            swap(nums[i], nums[j]);
+            permutations(nums, ans, i + 1, n);
+            swap(nums[i], nums[j]);
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int i = 0;
+        int n = nums.size();
+        permutations(nums, ans, 0, n);
+        return ans;
+    }
+};
