2777e6621302a154fbcc16ff9a2d7b0bfdc2f98f
S.Lakshmanan <d45624312@gmail.com>
2025-03-03 22:16:47 +0530

    Update codingday38.cpp

--- codingday38.cpp
+++ codingday38.cpp
@@ -32,3 +32,81 @@ public:
         return ans;
     }
 };
+/*Create Binary Tree From Descriptions
+You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+The test cases will be generated such that the binary tree is valid.
+Example 1:
+Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+Output: [50,20,80,15,17,19]
+Explanation: The root node is the node with value 50 since it has no parent.
+The resulting binary tree is shown in the diagram.
+Example 2:
+Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
+Output: [1,2,null,null,3,4]
+Explanation: The root node is the node with value 1 since it has no parent.
+The resulting binary tree is shown in the diagram.*/
+class Solution {
+public:
+    TreeNode* constructleft(unordered_map<int, TreeNode*>& mpp, TreeNode* node, TreeNode* left) {
+        node->left = left;
+        mpp[node->val] = node;
+        mpp[left->val] = left;
+        return node;
+    }
+
+    TreeNode* constructright(unordered_map<int, TreeNode*>& mpp, TreeNode* node, TreeNode* right) {
+        node->right = right;
+        mpp[node->val] = node;
+        mpp[right->val] = right;
+        return node;
+    }
+
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> mpp;
+        unordered_map<int, int> childMap;
+
+        for (int i = 0; i < descriptions.size(); i++) {
+            int parentVal = descriptions[i][0];
+            int childVal = descriptions[i][1];
+            int isRight = descriptions[i][2];
+
+            TreeNode* parent;
+            TreeNode* child;
+
+            if (mpp.find(parentVal) != mpp.end()) {
+                parent = mpp[parentVal];
+            } else {
+                parent = new TreeNode(parentVal);
+                mpp[parentVal] = parent;
+            }
+
+            if (mpp.find(childVal) != mpp.end()) {
+                child = mpp[childVal];
+            } else {
+                child = new TreeNode(childVal);
+                mpp[childVal] = child;
+            }
+
+            if (isRight == 1) {
+                constructleft(mpp, parent, child);
+            } else {
+                constructright(mpp, parent, child);
+            }
+
+            childMap[childVal] = 1;  
+        }
+
+        TreeNode* root = nullptr;
+        for (auto it : mpp) {
+            if (childMap.find(it.first) == childMap.end()) {
+                root = it.second;
+                break;
+            }
+        }
+
+        return root;
+    }
+};
