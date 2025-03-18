74b8f04c69ca1f6c747c11fd330abdf68d3aa818
S.Lakshmanan <d45624312@gmail.com>
2025-03-18 22:25:51 +0530

    Update codingday48.cpp

--- codingday48.cpp
+++ codingday48.cpp
@@ -64,3 +64,43 @@ public:
         return ans;
     }
 };
+/*Strictly Palindromic Number
+An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), the string representation of the integer n in base b is palindromic.
+Given an integer n, return true if n is strictly palindromic and false otherwise.
+A string is palindromic if it reads the same forward and backward.
+Example 1:
+Input: n = 9
+Output: false
+Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
+In base 3: 9 = 100 (base 3), which is not palindromic.
+Therefore, 9 is not strictly palindromic so we return false.
+Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
+Example 2:
+Input: n = 4
+Output: false
+Explanation: We only consider base 2: 4 = 100 (base 2), which is not palindromic.
+Therefore, we return false.*/
+class Solution {
+    string convert(int n,int i){
+        string temp; 
+        while(n){
+          temp+=to_string(n%i);
+          n=n/2;
+        }
+        return temp;
+    }
+    bool pal(string binary){
+        string temp=binary;
+        reverse(binary.begin(),binary.end());
+        return temp==binary;
+    }
+public:
+    bool isStrictlyPalindromic(int n) {
+        for(int i=2;i<=n-2;i++){
+            string binary=convert(n,i);
+            bool check=pal(binary);
+            if(!check){return false;}
+        }
+        return true;
+    }
+};
