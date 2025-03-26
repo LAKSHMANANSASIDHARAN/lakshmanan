93beab6ec5a0e1055a98f4d9efff53db6e9052e4
S.Lakshmanan <d45624312@gmail.com>
2025-03-26 20:17:57 +0530

    Update codingday54.cpp

--- codingday54.cpp
+++ codingday54.cpp
@@ -71,3 +71,39 @@ public:
         return res;
     }
 };
+/*Count the Number of Good Subarrays
+Given an integer array nums and an integer k, return the number of good subarrays of nums.
+A subarray arr is good if there are at least k pairs of indices (i, j) such that i < j and arr[i] == arr[j].
+A subarray is a contiguous non-empty sequence of elements within an array.
+Example 1:
+Input: nums = [1,1,1,1,1], k = 10
+Output: 1
+Explanation: The only good subarray is the array nums itself.
+Example 2:
+Input: nums = [3,1,4,3,2,2,4], k = 2
+Output: 4
+Explanation: There are 4 different good subarrays:
+- [3,1,4,3,2,2] that has 2 pairs.
+- [3,1,4,3,2,2,4] that has 3 pairs.
+- [1,4,3,2,2,4] that has 2 pairs.
+- [4,3,2,2,4] that has 2 pairs.*/
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        long long count = 0, ans = 0, n = nums.size();
+        int i = 0, j = 0;
+        unordered_map<int, int> mpp;
+        while (j < n) {
+            count += mpp[nums[j]];
+            mpp[nums[j]]++;
+            while (count >= k) {
+                ans += (n - j);
+                mpp[nums[i]]--;
+                count -= mpp[nums[i]];
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
