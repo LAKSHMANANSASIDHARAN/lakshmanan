b4aa87bf62d126e929bbeadc3ad18b09567509a3
S.Lakshmanan <d45624312@gmail.com>
2025-03-26 20:16:51 +0530

    Update codingday54.cpp

--- codingday54.cpp
+++ codingday54.cpp
@@ -38,3 +38,36 @@ public:
         return ans;
     }
 };
+/* Minimum Consecutive Cards to Pick Up
+You are given an integer array cards where cards[i] represents the value of the ith card. A pair of cards are matching if the cards have the same value.
+Return the minimum number of consecutive cards you have to pick up to have a pair of matching cards among the picked cards. If it is impossible to have matching cards, return -1.
+Example 1:
+Input: cards = [3,4,2,3,4,7]
+Output: 4
+Explanation: We can pick up the cards [3,4,2,3] which contain a matching pair of cards with value 3. Note that picking up the cards [4,2,3,4] is also optimal.
+Example 2:
+Input: cards = [1,0,5,3]
+Output: -1
+Explanation: There is no way to pick up a set of consecutive cards that contain a pair of matching cards.*/
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        unordered_map<int,int>mpp;
+        int count=0,res=INT_MAX;
+        int i=0,j=0,n=cards.size();
+        while(j<n){
+           mpp[cards[j]]++;
+           while(mpp[cards[j]]>=2){
+            res=min(res,j-i+1);
+            mpp[cards[i]]--;
+            i++;
+            count++;
+           }
+           j++;
+        }
+        if(count==0){
+            return -1;
+        }
+        return res;
+    }
+};
