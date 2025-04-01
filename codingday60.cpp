2e03d1c96c7bc68de94827c78037006849ee4f70
S.Lakshmanan <d45624312@gmail.com>
2025-04-01 22:12:47 +0530

    Update codingday60.cpp

--- codingday60.cpp
+++ codingday60.cpp
@@ -41,3 +41,56 @@ public:
         
     }
 };
+/*Reverse Words in a String
+Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+Example 1:
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.*/
+class Solution {
+public:
+    string reverseWords(string s) {
+      stack<string>st;
+      int i=0;
+    string temp="";
+       while(i<s.size()){
+        if(s[i]==' '){
+            if(!temp.empty()){
+            st.push(temp);
+            temp="";
+            }
+          
+        }else{
+        temp+=s[i];
+    
+        }
+        i++;
+       } 
+    if (!temp.empty()) {
+            st.push(temp);
+        }
+        string ans="";
+        while(!st.empty()){
+            if(ans==""){
+                ans+=st.top();
+                st.pop();
+            }
+            else{
+                ans+=" ";
+                ans+=st.top();
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
