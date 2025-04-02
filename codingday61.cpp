79544b517460eef505d0b92bce5b9718c6a1c0a6
S.Lakshmanan <d45624312@gmail.com>
2025-04-02 22:04:55 +0530

    Update codingday61.cpp

--- codingday61.cpp
+++ codingday61.cpp
@@ -88,3 +88,34 @@ public:
         }
     }
 };
+/*Move Zeroes
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+Input: nums = [0]
+Output: [0]*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int l =0,r=0;
+       while(r<nums.size()){
+        if(nums[l]==0){
+            while(r<nums.size() and nums[r]==0 ){
+                r++;
+            }
+            if(r<nums.size()){
+            swap(nums[l],nums[r]);
+            }
+            l++;
+            r=l;
+        }
+        else{
+            l++;r++;
+        }
+       } 
+       
+    }
+};
