3dccbe022148fd806db39c7559b575c7c631b485
S.Lakshmanan <d45624312@gmail.com>
2025-04-03 22:29:43 +0530

    Update codingday62.cpp

--- codingday62.cpp
+++ codingday62.cpp
@@ -53,3 +53,39 @@ public:
         
     }
 };
+/*Reverse String II
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+Example 1:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Example 2:
+Input: s = "abcd", k = 2
+Output: "bacd"
+ */
+class Solution {
+    void reversestring(string &s,int i,int j){
+         
+         while(i<j){
+            swap(s[i],s[j]);
+            i++;j--;
+         }
+    }
+public:
+    string reverseStr(string &s, int k) {
+        int i=0,j=2*k-1;
+        while(j<s.size()){
+           reversestring(s,i,i+k-1);
+           i=j+1;
+           j+=2*k;
+        }
+        int n=s.size();
+        if(i<n && n-i<k){
+           reversestring(s,i,n-1);
+        }
+        else if(i<n && n-i<2*k && n-i>=k){
+            reversestring(s,i,i+k-1);
+        }
+        return s;
+    }
+};
