d55f416866694499992bf7164cde4d63f3df570b
S.Lakshmanan <d45624312@gmail.com>
2025-04-09 16:47:11 +0530

    Update codingday67.cpp

--- codingday67.cpp
+++ codingday67.cpp
@@ -67,3 +67,40 @@ public:
     }
        
 };
+/*Minimum Absolute Difference
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+Example 1:
+Input: arr = [4,2,1,3]
+Output: [[1,2],[2,3],[3,4]]
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+Example 2:
+Input: arr = [1,3,6,10,15]
+Output: [[1,3]]
+Example 3:
+Input: arr = [3,8,-10,23,19,-4,-14,27]
+Output: [[-14,-10],[19,23],[23,27]]*/
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end());
+        int temp = abs(arr[0] - arr[1]);
+        ans.push_back({arr[0], arr[1]});
+     for (int i = 1; i < arr.size() - 1; i++) {
+            int diff = abs(arr[i] - arr[i + 1]);
+            if (diff < temp) {
+                temp = diff;
+                ans.clear();
+                ans.push_back({arr[i], arr[i + 1]});
+            } else if (diff == temp) {
+                ans.push_back({arr[i], arr[i + 1]});
+            }
+        }
+
+        return ans;
+    }
+};
