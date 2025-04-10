5ed305c8156df567f7c7b066a416893e4b4e0eee
S.Lakshmanan <d45624312@gmail.com>
2025-04-10 20:03:38 +0530

    Update codingday68.cpp

--- codingday68.cpp
+++ codingday68.cpp
@@ -124,3 +124,35 @@ public:
         return max(maxi,maxi2);
     }
 };
+/*Merge Intervals
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.*/
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        int n = intervals.size() - 1;
+        int first = intervals[0][0], second = intervals[0][1];
+        int i = 1;
+        while (i <= n) {
+            if (intervals[i][0] <= second) {
+                second = max(second, intervals[i][1]);
+            } else {
+                ans.push_back({first, second});
+                first = intervals[i][0];
+                second = intervals[i][1];
+            }
+            i++;
+        }
+        ans.push_back({first, second});
+        return ans;
+    }
+};
