9ba00e85dd9b63a1448bec9ae47e0cb885bcf407
S.Lakshmanan <d45624312@gmail.com>
2025-04-24 22:00:39 +0530

    Update codingday79.cpp

--- codingday79.cpp
+++ codingday79.cpp
@@ -115,3 +115,40 @@ public:
         return res;
     }
 };
+/*Count Binary Substrings
+Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+Substrings that occur multiple times are counted the number of times they occur.
+Example 1:
+Input: s = "00110011"
+Output: 6
+Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+Notice that some of these substrings repeat and are counted the number of times they occur.
+Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+Example 2:
+Input: s = "10101"
+Output: 4
+Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.*/
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+         vector<int>arr;
+         int i=1;
+         int count=1;
+        while(i<s.size()){
+            if(s[i]==s[i-1]){
+                count++;
+            }
+            else{
+                arr.push_back(count);
+                count=1;
+            }
+            i++;
+        }
+          arr.push_back(count);
+        int res=0;
+        for(int i=0;i<arr.size()-1;i++){
+            res+=min(arr[i],arr[i+1]);
+        }
+        return res;
+    }
+};
