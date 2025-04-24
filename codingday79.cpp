bcd5906baa138070a1ab333773320e90562b464d
S.Lakshmanan <d45624312@gmail.com>
2025-04-24 21:58:17 +0530

    Create codingday79.cpp

---
+++ codingday79.cpp
@@ -0,0 +1,41 @@
+/*Count Complete Subarrays in an Array
+You are given an array nums consisting of positive integers.
+We call a subarray of an array complete if the following condition is satisfied:
+The number of distinct elements in the subarray is equal to the number of distinct elements in the whole array.
+Return the number of complete subarrays.
+A subarray is a contiguous non-empty part of an array.
+Example 1:
+Input: nums = [1,3,1,2,2]
+Output: 4
+Explanation: The complete subarrays are the following: [1,3,1,2], [1,3,1,2,2], [3,1,2] and [3,1,2,2].
+Example 2:
+Input: nums = [5,5,5,5]
+Output: 10
+Explanation: The array consists only of the integer 5, so any subarray is complete. The number of subarrays that we can choose is 10.*/
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+       unordered_map<int,int>temp,check;
+       for(int i:nums){
+        check[i]++;
+       }
+       int i=0,j=0,count=0;
+       while(j<nums.size()){
+          temp[nums[j]]++;
+        while(i<=j&&check.size()==temp.size()){
+              count+=nums.size()-j;
+              temp[nums[i]]--;
+              if(temp[nums[i]]==0){
+                temp.erase(nums[i]);
+              }
+              i++;
+        }
+      
+        j++;
+       }
+       return count;
+
+
+
+    }
+};
