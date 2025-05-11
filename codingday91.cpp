f81140b38bacf4d029df98f4ce9bd57e58dd193d
S.Lakshmanan <d45624312@gmail.com>
2025-05-11 10:13:11 +0530

    Update codingday91.cpp

--- codingday91.cpp
+++ codingday91.cpp
@@ -39,3 +39,52 @@ public:
 
     }
 };
+/*Find Valid Pair of Adjacent Digits in String
+You are given a string s consisting only of digits. A valid pair is defined as two adjacent digits in s such that:
+The first digit is not equal to the second.
+Each digit in the pair appears in s exactly as many times as its numeric value.
+Return the first valid pair found in the string s when traversing from left to right. If no valid pair exists, return an empty string.
+Example 1:
+Input: s = "2523533"
+Output: "23"
+Explanation:
+Digit '2' appears 2 times and digit '3' appears 3 times. Each digit in the pair "23" appears in s exactly as many times as its numeric value. Hence, the output is "23".
+Example 2:
+Input: s = "221"
+Output: "21"
+Explanation:
+Digit '2' appears 2 times and digit '1' appears 1 time. Hence, the output is "21".
+Example 3:
+Input: s = "22"
+Output: ""
+Explanation:
+There are no valid adjacent pairs.*/
+class Solution {
+    bool is(char f,char t, vector<int>&hash){
+        int num1=f-'0',num2=t-'0';
+        if(num1==hash[f-'0']&&num2==hash[t-'0']){
+            return true;
+        }
+        return false;
+    }
+public:
+    string findValidPair(string s) {
+        vector<int>hash(10,0);
+        for(auto it:s){
+            hash[it-'0']++;
+        }
+        string ans="";
+        int f=0,t=1;
+        while(t<s.size()){
+            if(s[f]!=s[t]&&is(s[f],s[t],hash)){
+               ans+=s[f];
+               ans+=s[t];
+               break; 
+            }
+            f++;
+            t++;
+        }
+        return ans;
+        
+    }
+};
