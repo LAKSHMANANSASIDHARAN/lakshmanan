d9d4742d00d075b830d3a49356df3b72be065cbb
S.Lakshmanan <d45624312@gmail.com>
2025-05-24 21:54:34 +0530

    Update codingday101.cpp

--- codingday101.cpp
+++ codingday101.cpp
@@ -49,3 +49,54 @@ public:
        return true;
     }
 };
+/*Valid Mountain Array
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+Recall that arr is a mountain array if and only if:
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Example 1:
+Input: arr = [2,1]
+Output: false
+Example 2:
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+Input: arr = [0,3,2,1]
+Output: true*/
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        if(arr.size()<3){
+            return false;
+        }
+       int i=1;
+       bool ch=false;
+       while(i<arr.size()-1){
+        if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]){
+            ch=true;
+          break;
+        }
+       
+        i++;
+       }
+       if(!ch){
+        return false;
+       }
+       int left=i-1,right=i+1;
+       while(left>=0){
+        if(arr[left]>=arr[left+1]){
+            return false;
+        }
+        left--;
+       }
+       while(right<arr.size()){
+         if(arr[right]>=arr[right-1]){
+            return false;
+        }
+        right++;
+       }
+       return true;
+    }
+};
