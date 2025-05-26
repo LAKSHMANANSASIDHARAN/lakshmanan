0035a32676df1b8edfa4f7cf5acfcd1ac787ecb8
S.Lakshmanan <d45624312@gmail.com>
2025-05-26 22:07:28 +0530

    Update codingday103.cpp

--- codingday103.cpp
+++ codingday103.cpp
@@ -57,3 +57,38 @@ for(auto &[f,s]:mp){
       return count; 
     }
 };
+/*Check if Every Row and Column Contains All Numbers
+An n x n matrix is valid if every row and every column contains all the integers from 1 to n (inclusive).
+Given an n x n integer matrix matrix, return true if the matrix is valid. Otherwise, return false.
+Example 1:
+Input: matrix = [[1,2,3],[3,1,2],[2,3,1]]
+Output: true
+Explanation: In this case, n = 3, and every row and column contains the numbers 1, 2, and 3.
+Hence, we return true.
+Example 2:
+Input: matrix = [[1,1,1],[1,2,3],[1,2,3]]
+Output: false
+Explanation: In this case, n = 3, but the first row and the first column do not contain the numbers 2 or 3.
+Hence, we return false.*/
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        vector<int>temp;
+        for(int i=1;i<=matrix.size();i++){
+            temp.push_back(i);
+        }
+        for(int i=0;i<matrix.size();i++){
+            vector<int>row,col;
+           for(int j=0;j<matrix.size();j++){
+                row.push_back(matrix[i][j]);
+                col.push_back(matrix[j][i]);
+           }
+           sort(row.begin(),row.end());
+           sort(col.begin(),col.end());
+           if(row!=temp or col !=temp){
+            return false;
+           }
+        }
+        return true;
+    }
+};
