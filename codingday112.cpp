7fd79eea7437a4ab721c50fbcd037074793e908e
S.Lakshmanan <d45624312@gmail.com>
2025-06-04 19:02:14 +0530

    Update codingday112.cpp

--- codingday112.cpp
+++ codingday112.cpp
@@ -60,3 +60,38 @@ public:
         return res;
     }
 };
+/*Check If Two String Arrays are Equivalent
+Solved
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+A string is represented by an array if the array elements concatenated in order forms the string.
+Example 1:
+Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.
+Example 2:
+Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+Output: false
+Example 3:
+Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+Output: true*/
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string temp1="",temp2="";
+        int i=0,j=0;
+        while(i<word1.size() || j<word2.size()){
+            if(i<word1.size()){
+                temp1+=word1[i];
+                i++;
+            }
+            if(j<word2.size()){
+                temp2+=word2[j];
+                j++;
+            }
+        }
+        return temp1==temp2;
+    }
+};
