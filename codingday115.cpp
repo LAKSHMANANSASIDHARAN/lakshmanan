d3b694f673253cd1f98f6cd42154db6248212fb7
S.Lakshmanan <d45624312@gmail.com>
2025-06-07 19:03:08 +0530

    Update codingday115.cpp

--- codingday115.cpp
+++ codingday115.cpp
@@ -41,3 +41,45 @@ public:
         return ans;
     }
 };
+/*Minimum Length of String After Operations
+You are given a string s.
+You can perform the following process on s any number of times:
+Choose an index i in the string such that there is at least one character to the left of index i that is equal to s[i], and at least one character to the right that is also equal to s[i].
+Delete the closest occurrence of s[i] located to the left of i.
+Delete the closest occurrence of s[i] located to the right of i.
+Return the minimum length of the final string s that you can achieve.
+Example 1:
+Input: s = "abaacbcbb"
+Output: 5
+Explanation:
+We do the following operations:
+Choose index 2, then remove the characters at indices 0 and 3. The resulting string is s = "bacbcbb".
+Choose index 3, then remove the characters at indices 0 and 5. The resulting string is s = "acbcb".
+Example 2:
+Input: s = "aa"Output: 2
+Explanation:
+We cannot perform any operations, so we return the length of the original string.*/
+class Solution {
+public:
+    int minimumLength(string s) {
+     unordered_map<char,int>map;
+        int len=0;
+        for(int i=0;i<s.size();i++){
+            map[s[i]]++;
+        }
+        for(auto i:map){
+            if(i.second>2){
+                int temp=i.second;
+               while(temp>2){
+                   temp-=2;
+               }
+               len+=temp;
+            }
+            else{
+                len+=i.second;
+            }
+        }
+        if(len==0) return s.size();
+        return len;
+    }
+};
