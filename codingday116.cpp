eb0f1494883c825a69bdb4192bb5f769184e556e
S.Lakshmanan <d45624312@gmail.com>
2025-06-08 21:15:02 +0530

    Update codingday116.cpp

--- codingday116.cpp
+++ codingday116.cpp
@@ -87,3 +87,34 @@ public:
      return true;
     }
 };
+/*Maximum Nesting Depth of the Parentheses
+Given a valid parentheses string s, return the nesting depth of s. The nesting depth is the maximum number of nested parentheses.
+Example 1:
+Input: s = "(1+(2*3)+((8)/4))+1"
+Output: 3
+Explanation:
+Digit 8 is inside of 3 nested parentheses in the string.
+Example 2:
+Input: s = "(1)+((2))+(((3)))"
+Output: 3
+Explanation:
+Digit 3 is inside of 3 nested parentheses in the string.
+Example 3:
+Input: s = "()(())((()()))"
+Output: 3*/
+class Solution {
+public:
+    int maxDepth(string s) {
+        int open=0,maxi=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                open++;
+            }
+            else if(s[i]==')'){
+                open--;
+            }
+            maxi=max(open,maxi);
+        }
+        return maxi;
+    }
+};
