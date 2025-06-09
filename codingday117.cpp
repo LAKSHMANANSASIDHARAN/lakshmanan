0ff77fc4228efd63e6ee20ced2ecb712050823fd
S.Lakshmanan <d45624312@gmail.com>
2025-06-09 20:51:43 +0530

    Update codingday117.cpp

--- codingday117.cpp
+++ codingday117.cpp
@@ -90,3 +90,28 @@ public:
       return c;
     }
 };
+/*Find All Numbers Disappeared in an Array
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+ */
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+     unordered_set<int> check;
+     for(auto i:nums){
+        check.insert(i);
+     }  
+     vector<int>res;
+     for(int i=1;i<=nums.size();i++){
+        if(check.find(i)==check.end()){
+            res.push_back(i);
+        }
+     }
+     return res;
+    }
+};
