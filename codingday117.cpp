f770e277f42ac73ec53a4cb1982ec7b23469f969
S.Lakshmanan <d45624312@gmail.com>
2025-06-09 20:53:21 +0530

    Update codingday117.cpp

--- codingday117.cpp
+++ codingday117.cpp
@@ -115,3 +115,50 @@ public:
      return res;
     }
 };
+/* Maximum Number of Words You Can Type
+There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+Example 1:
+Input: text = "hello world", brokenLetters = "ad"
+Output: 1
+Explanation: We cannot type "world" because the 'd' key is broken.
+Example 2:
+Input: text = "leet code", brokenLetters = "lt"
+Output: 1
+Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+Example 3:
+Input: text = "leet code", brokenLetters = "e"
+Output: 0
+Explanation: We cannot type either word because the 'e' key is broken.*/
+class Solution {
+    bool check(string s, unordered_set<char>st){
+        for(auto i:s){
+            if(st.find(i)!=st.end()) return false;
+        }
+        return true;
+    }
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_set<char>st;
+        for(auto i:brokenLetters){
+            st.insert(i);
+        }
+        string temp="";
+        int count=0;
+        for(int i=0;i<text.size();i++){
+        if(text[i]==' '){
+          if(check(temp,st)){
+            count++;
+          } 
+          temp=""; 
+        }
+        else{
+            temp+=text[i];
+        }
+        }
+      if(check(temp,st)){
+            count++;
+          } 
+          return count;
+    }
+};
