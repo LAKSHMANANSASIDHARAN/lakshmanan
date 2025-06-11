da541362fea737457bcb183f686ff35f336ed08d
S.Lakshmanan <d45624312@gmail.com>
2025-06-11 20:23:17 +0530

    Update codingday118.cpp

--- codingday118.cpp
+++ codingday118.cpp
@@ -41,3 +41,40 @@ public:
         return {ans1,ans2};
     }
 };
+/* Find the XOR of Numbers Which Appear Twice
+You are given an array nums, where each number in the array appears either once or twice.
+Return the bitwise XOR of all the numbers that appear twice in the array, or 0 if no number appears twice.
+Example 1:
+Input: nums = [1,2,1,3]
+Output: 1
+Explanation:
+The only number that appears twice in nums is 1.
+Example 2:
+Input: nums = [1,2,3]
+Output: 0
+Explanation:
+No number appears twice in nums.
+Example 3:
+Input: nums = [1,2,2,1]
+Output: 3
+Explanation:
+Numbers 1 and 2 appeared twice. 1 XOR 2 == 3.*/
+class Solution {
+public:
+    int duplicateNumbersXOR(vector<int>& nums) {
+       vector<int>temp;
+       unordered_map<int,int>mp;
+       for(auto i:nums){
+        mp[i]++;
+       } 
+       for(auto i:mp){
+        if(i.second==2){
+            temp.push_back(i.first);
+        }
+       }
+    
+       int count=0;
+       for(auto i:temp) count^=i;
+       return count;
+    }
+};
