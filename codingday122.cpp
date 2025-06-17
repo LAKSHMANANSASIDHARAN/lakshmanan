98a1948a552585b539f56c1bba031257492f3aa8
S.Lakshmanan <d45624312@gmail.com>
2025-06-17 19:21:18 +0530

    Update codingday122.cpp

--- codingday122.cpp
+++ codingday122.cpp
@@ -27,3 +27,50 @@ public:
             return count;
     }
 };
+/*Sort List
+Given the head of a linked list, return the list after sorting it in ascending order.
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+Input: head = []
+Output: []
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr or head->next==nullptr){
+            return head;
+        }
+        vector<int>arr;
+        ListNode*temp=head;
+        while(temp!=nullptr){
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(arr.begin(),arr.end());
+        int i=0;
+        temp=head;
+        while(temp!=nullptr){
+            temp->val=arr[i];
+            i++;
+            temp=temp->next;
+        }
+      //  delete temp;
+        return head;
+        
+    }
+};
