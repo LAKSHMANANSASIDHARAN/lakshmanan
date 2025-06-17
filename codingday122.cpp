54e00e78c542295d80fa725dfe620f12861bc40c
S.Lakshmanan <d45624312@gmail.com>
2025-06-17 19:22:12 +0530

    Update codingday122.cpp

--- codingday122.cpp
+++ codingday122.cpp
@@ -74,3 +74,31 @@ public:
         
     }
 };
+/* Kth Largest Element in an Array
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Can you solve it without sorting?
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+     sort(nums.begin(),nums.end());
+      int i=nums.size()-1;
+      int res;
+      int count=1;
+      while(i>=0){
+        if(count==k){
+            res=nums[i];
+            break;
+        }
+        i--;
+        count++;
+     }
+     return res;
+    }
+};
