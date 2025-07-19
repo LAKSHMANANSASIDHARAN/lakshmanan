04ada58d9e3718da4846de423a99c0ed3b5bff14
S.Lakshmanan <d45624312@gmail.com>
2025-07-19 22:10:09 +0530

    Update codingday139.cpp

--- codingday139.cpp
+++ codingday139.cpp
@@ -35,3 +35,44 @@ public:
         return ans;
     }
 };
+/*1346. Check If N and Its Double Exist
+Given an array arr of integers, check if there exist two indices i and j such that :
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+ Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.*/
+class Solution {
+    bool check(int i, vector<int>& arr) {
+    int target = 2 * arr[i];
+    int left = 0, right = arr.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (mid == i) {
+            if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        else if (arr[mid] < target) left = mid + 1;
+        else if (arr[mid] > target) right = mid - 1;
+        else return true;
+    }
+    return false;
+}
+
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if(check(i,arr)) return true;
+        }
+        return false;
+    }
+};
