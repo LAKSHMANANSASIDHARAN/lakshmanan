aa7440203c29ac93ea640ac7e2acdcc71d63f449
S Lakshmanan <d45624312@gmail.com>
2025-08-15 12:44:40 +0530

    Create codingday153.cpp

---
+++ codingday153.cpp
@@ -0,0 +1,34 @@
+/*77. Combinations
+Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+You may return the answer in any order.
+Example 1:
+Input: n = 4, k = 2
+Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+Explanation: There are 4 choose 2 = 6 total combinations.
+Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+Example 2:
+Input: n = 1, k = 1
+Output: [[1]]
+Explanation: There is 1 choose 1 = 1 total combination.*/
+class Solution {
+  void  combination(int i,vector<vector<int>>&ans,vector<int>&temp,int n,int k){
+        if(temp.size()==k){
+            ans.push_back(temp);
+            return ;
+        } 
+        if(i>n){
+            return ;
+        }
+        temp.push_back(i);
+        combination(i+1,ans,temp,n,k);
+        temp.pop_back();
+        combination(i+1,ans,temp,n,k);
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+       vector<vector<int>>ans;
+       vector<int>temp;
+       combination(1,ans,temp,n,k);
+       return ans; 
+    }
+};
