3cce4ff489b285fc5ead5dc4e018ac674f0ee8b3
S Lakshmanan <d45624312@gmail.com>
2025-08-15 12:46:02 +0530

    Update codingday153.cpp

--- codingday153.cpp
+++ codingday153.cpp
@@ -32,3 +32,31 @@ public:
        return ans; 
     }
 };
+/*90. Subsets II
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+Example 1:
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]*/
+class Solution {
+    void subset(vector<vector<int>>& ans, vector<int>& temp, vector<int>& nums, int i) {
+      ans.push_back(temp); 
+        for (int j = i; j < nums.size(); j++) {
+            if (j > i && nums[j] == nums[j - 1]) continue; 
+            temp.push_back(nums[j]); 
+            subset(ans, temp, nums, j + 1);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        subset(ans, temp, nums, 0);
+        return ans;
+    }
+};
