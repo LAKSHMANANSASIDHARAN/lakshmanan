8bcb1f41b7c9335f2d6b99bd6192ea7a401d13eb
S Lakshmanan <d45624312@gmail.com>
2025-09-18 18:48:47 +0530

    Update codingday175.cpp

--- codingday175.cpp
+++ codingday175.cpp
@@ -37,3 +37,41 @@ public:
         return ans;
     }
 };
+/*988. Smallest String Starting From Leaf
+You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+As a reminder, any shorter prefix of a string is lexicographically smaller.
+For example, "ab" is lexicographically smaller than "aba".
+A leaf of a node is a node that has no children.
+Example 1:
+Input: root = [0,1,2,3,4,3,4]
+Output: "dba"
+Example 2:
+Input: root = [25,1,3,1,3,0,2]
+Output: "adz"
+Example 3:
+Input: root = [2,2,1,null,1,0,null,0]
+Output: "abc"*/
+class Solution {
+    void res(TreeNode* root, string& ans, string& temp, bool parentIsLeaf) {
+        if (!root) {
+            if (parentIsLeaf) {                    
+                string t = temp;
+                reverse(t.begin(), t.end());
+                if (ans.empty() || t < ans) ans = t;
+            }
+            return;
+        }
+        temp += 'a' + root->val;
+        bool leaf = !root->left && !root->right;    
+        res(root->left,  ans, temp, leaf);
+        res(root->right, ans, temp, leaf);
+        temp.pop_back();
+    }
+public:
+    string smallestFromLeaf(TreeNode* root) {
+        string ans, temp;
+        res(root, ans, temp, false);
+        return ans;
+    }
+};
