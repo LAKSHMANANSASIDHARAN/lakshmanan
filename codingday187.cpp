e514cf92c85b25947cbd0cc47420482993646b53
S Lakshmanan <d45624312@gmail.com>
2025-10-05 19:03:13 +0530

    Update codingday187.cpp

--- codingday187.cpp
+++ codingday187.cpp
@@ -61,3 +61,45 @@ public:
         return count;
     }
 };
+/*1329. Sort the Matrix Diagonally
+A matrix diagonal is a diagonal line of cells starting from some cell in either the topmost row or leftmost column and going in the bottom-right direction until reaching the matrix's end. For example, the matrix diagonal starting from mat[2][0], where mat is a 6 x 3 matrix, includes cells mat[2][0], mat[3][1], and mat[4][2].
+Given an m x n matrix mat of integers, sort each matrix diagonal in ascending order and return the resulting matrix.
+Example 1:
+Input: mat = [[3,3,1,1],[2,2,1,2],[1,1,1,2]]
+Output: [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+Example 2:
+Input: mat = [[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]]
+Output: [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]*/
+class Solution {
+    vector<int>process(int i, int j, vector<vector<int>>& nums){
+        vector<int>temp;
+        while(i<nums.size() && j<nums[0].size()){
+         temp.push_back(nums[i][j]);
+         i++;
+         j++;
+        }
+        sort(temp.begin(),temp.end());
+        return temp;
+    }
+    void correct(vector<int>&temp, int i, int j,vector<vector<int>>& nums){
+        int ind=0;
+         while(i<nums.size() && j<nums[0].size()){
+         nums[i][j]=temp[ind];
+         i++;
+         j++;
+         ind++;
+        }
+    }
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        for(int i=0;i<mat.size();i++){
+           vector<int>temp= process(i,0,mat);
+           correct(temp,i,0,mat);
+        }
+        for(int i=1;i<mat[0].size();i++){
+            vector<int>temp= process(0,i,mat);
+           correct(temp,0,i,mat);
+        }
+        return mat;
+    }
+};
